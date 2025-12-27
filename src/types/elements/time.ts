5bccd6e983a51dbbd7b5e64c4c26af8301a4eb10
muyids <289869876@qq.com>
2025-12-27 19:14:56 +0800

    fix font location

--- src/types/elements/time.ts
+++ src/types/elements/time.ts
@@ -3,6 +3,8 @@ import type { TextElementConfig } from './text'
 export interface TimeElementConfig extends TextElementConfig {
   eleType: 'time'
   formatter: number
+  // 仅用于导出：文字 baseline 的纵坐标
+  topBase?: number
 }
 
 export interface DateElementConfig extends TextElementConfig {
